48c2a3c10a68b4cd2f14f912c7aa542582cc8b51
Marko Urbanč <urbancmarko1@gmail.com>
2023-09-09 17:30:38 +0200

    feat: :construction: work on flow solution

--- src.py
+++ src.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cmasher as cmr
 from scipy.special import beta
+from time import perf_counter_ns
 
 
 plt.rcParams["font.family"] = "BigBlueTerm437 Nerd Font Mono"
@@ -14,6 +15,7 @@ plt.rcParams['mathtext.rm'] = 'BigBlueTerm437 Nerd Font Mono'
 plt.rcParams['mathtext.it'] = 'BigBlueTerm437 Nerd Font Mono:italic'
 plt.rcParams['mathtext.bf'] = 'BigBlueTerm437 Nerd Font Mono:bold'
 plt.rcParams['mathtext.sf'] = 'BigBlueTerm437 Nerd Font Mono'
+plt.rcParams.update({'axes.unicode_minus' : False})
 
 # Text color #97FC1A
 # ['#000000', '#11140B', '#23301C', '#2F4A2A', '#356636', '#368440', '#30A245', '#32C141', '#5EE032', '#97FC1A']
@@ -32,6 +34,12 @@ class GalerkinObject():
         self.N_dim = N_dim
         # Sub colormap for better contrast on our quasi-green screen
         self.cmap = cmr.get_sub_cmap('cmr.nuclear', 0.2, 0.8)
+        # Init empty variables
+        self.basis = None
+        self.A = None
+        self.b = None
+        self.a = None
+        self.Z = None
     
     def basis_function(self, x, phi, m, n):
         """
@@ -115,19 +123,53 @@ class GalerkinObject():
 
         self.C *= -32/np.pi 
 
-        return self.C             
+        return self.C       
+
+    def _solve_a(self):
+        """
+        Solve for flow field coefficients a_mn.
+        """
+        if self.basis is None:
+            self.create_basis(self.M_dim, self.N_dim)
+        if self.A is None:
+            self._create_matrix(self.M_dim, self.N_dim)
+        if self.b is None:
+            self._create_vector(self.M_dim, self.N_dim)
+        
+        self.a = np.zeros(self.M_dim*self.N_dim)
+        A_inv = np.linalg.inv(self.A)
+        for m in range(self.M_dim):
+            for n in range(1, self.N_dim + 1):
+                for m_prime in range(self.M_dim):
+                    for n_prime in range(1, self.N_dim + 1):
+                        self.a[m_prime*self.N_dim + n_prime - 1] += A_inv[m * self.N_dim + n - 1, m_prime * self.N_dim + n_prime - 1] * \
+                                                                    self.b[m*self.N_dim + n - 1]
+
+        return self.a
     
-    def plot_flow(self, Z: np.ndarray | None):
+    def solve_flow(self):
+        """
+        Solve for the flow field.
+        """
+        if self.a is None:
+            self._solve_a()
+        
+        # self.Z = np.zeros((self.M_dim, self.N_dim, self.X.shape[0], self.X.shape[1]))
+        self.Z = 0
+        for m in range(self.M_dim):
+            for n in range(1, self.N_dim + 1):
+                self.Z += self.a[m*self.N_dim + n - 1] * self.basis_function(self.X, self.PHI, m, n)
+        
+        return self.Z
+    
+    def plot_flow(self, Z: np.ndarray | None, m: int = None, n: int = None, title: str = None):
         fig, ax = plt.subplots(subplot_kw=dict(projection='polar'), facecolor="#000000")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_thetalim(0, np.pi)
 
-        m = self.m
-        n = self.n
-
         if Z is None:
-            Z = self.basis_function(self.X, self.PHI, self.m, self.n)
+            Z = self.basis_function(self.X, self.PHI, m, n)
 
         # Plot the contourf with the colormap
         cnt = ax.contourf(self.PHI, self.X, Z, cmap=self.cmap)
@@ -165,12 +207,36 @@ class GalerkinObject():
         ax.spines['start'].set_alpha(0.8)
         ax.spines['start'].set_linestyle("--")
 
-        plt.title(f"Basis function $m={m}$, $n={n}$", color="#5EE032")
+        plt.title(f"{title} $m={m}$, $n={n}$", color="#5EE032")
         plt.grid(False)
         plt.savefig('filename.png', dpi=1200, bbox_inches="tight")
         plt.show()
 
-GO = GalerkinObject(2, 2)
 
-mat = GO.solve()
-print(mat)
+def scale_it(N_range, M_range):
+    """
+    Scale the basis functions to see how they change with the 
+    number of basis functions.
+    """
+    solutions = {}
+    for M in range(1, M_range + 1):
+        for N in range(1, N_range + 1):
+            GO = GalerkinObject(M, N)
+            print(f"Now solving for basis of size m={M}, n={N}")
+            time = perf_counter_ns()
+            C = GO.solve()
+            time = (perf_counter_ns() - time)
+            solutions[f"m={M}, n={N}"] = (C, time)
+
+    return solutions
+
+# print(scale_it(5, 5))
+
+m = 0
+n = 1
+GO = GalerkinObject(3, 3)
+val = GO.solve()
+print(val)
+Z = GO.solve_flow()
+GO.plot_flow(Z, m, n, f"Flow Field $C = {round(val, 3)}$")
+
